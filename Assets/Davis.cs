c0ec6b96bb5180ec26fa117ddba917bc53311f8b
kocochj999 <kocochj999@gmail.com>
2020-05-23 12:55:55 +0700

    update Player_hurt + Enemy_AI

---
+++ Assets/Davis.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Davis : MonoBehaviour
+{
+    [SerializeField] private float leftPoint;
+    [SerializeField] private float rightPoint;
+
+    private float speed;
+    private bool facingRight = true;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log(anim.GetBool("isWalking"));
+        if (anim.GetBool("isWalking"))
+        {
+            if (Mathf.Abs(rb.velocity.x) < 0.1f)
+            {
+                anim.SetBool("isWalking", false);
+            }
+        }
+
+    }
+
+    private void Move()
+    {
+        if (facingRight)
+        {
+            
+
+            if (transform.position.x < rightPoint)
+            {
+                if (transform.localScale.x != 1)
+                {
+                    transform.localScale = new Vector3(1, 1);
+                }
+                anim.SetBool("isWalking", true);
+                //Debug.Log(anim.GetBool("isWalking"));
+                rb.velocity = new Vector2(3f, rb.velocity.y);
+                
+            }
+            else
+            {
+                facingRight = false;
+            }
+        }
+        else
+        {
+            
+            
+            
+            if (transform.position.x > leftPoint)
+            {
+                if (transform.localScale.x != -1)
+                {
+                    transform.localScale = new Vector3(-1, 1);
+                }
+                anim.SetBool("isWalking", true);
+                //Debug.Log(anim.GetBool("isWalking"));
+                rb.velocity = new Vector2(-3f, rb.velocity.y);
+                
+            }
+            else
+            {
+                facingRight = true;
+            }
+        }
+    }
+    public void JumpedOn()
+    {
+        anim.SetTrigger("Dead");
+    }
+    private void Dead()
+    {
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Resources;
 using UnityEditor.ShortcutManagement;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,13 +14,13 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
     private Collider2D coll;
-    private bool isRunning =false;
+    private bool isRunning = false;
     private bool isWalking = false;
 
     // State varibles a.k.a Finite State Machine
-    private enum State { indle, walking, running, jumping, falling}
+    private enum State { indle, walking, running, jumping, falling, hurt }
     private State state = State.indle;
-    
+
     // Double tap chekcer varibles
     float buttonCooler = 0.5f;
     int buttonCountA = 0;
@@ -27,10 +28,11 @@ public class PlayerController : MonoBehaviour
 
     // Inspector Varibles
     [SerializeField] private LayerMask ground;
-    [SerializeField] private float speed = 3f;
+    [SerializeField] private float speed;
     [SerializeField] private float jumpHeight = 7f;
     [SerializeField] private int coin = 0;
     [SerializeField] private Text coinQuantity;
+    [SerializeField] private float hurtForce = 4f;
 
     // Start is called before the first frame update
     void Start()
@@ -48,10 +50,12 @@ public class PlayerController : MonoBehaviour
 
 
         //Debug.Log(buttonCount);
-        
-        Debug.Log(state);
 
-        InputHandle();
+        
+        if (state != State.hurt)
+        {
+            InputHandle();
+        }
         StateSwitch();
         anim.SetInteger("state", (int)state); // Set state for enum 
 
@@ -61,35 +65,70 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if( collision.tag == "Collectible")
+        if (collision.tag == "Collectible")
         {
             Destroy(collision.gameObject);
             coin += 1;
             coinQuantity.text = coin.ToString();
-            
+
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Davis davis = collision.gameObject.GetComponent<Davis>(); 
+            isRunning = false;
+            isWalking = false;
+            if (state == State.falling)
+            {
+                davis.JumpedOn();
+                Jumping();
+            }
+            else
+            {
+
+                //  be hurt until indle
+                // 
+
+                state = State.hurt;
+                
+               
+                if (collision.gameObject.transform.position.x > gameObject.transform.position.x)
+                {
+                    rb.velocity = new Vector2(-hurtForce, rb.velocity.y);
+                }
+                else
+                {
+                    rb.velocity = new Vector2(hurtForce, rb.velocity.y);
+                }
+
+
+
+            }
+        }
+
     }
     private void InputHandle()
     {
-        
+
         // Jump
         if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
-            state = State.jumping;
+            Jumping();
         }
-        
+
         if (isRunning)
         {
             Movement(6f);
-            
+
 
         }
         else if (isWalking)
         {
             Movement(3f);
         }
-        
+
         // Check walking or running
         if (Input.GetKeyDown(KeyCode.A))  // Input.GetKeyDown(KeyCode.A) hDirection < 0
         {
@@ -112,7 +151,47 @@ public class PlayerController : MonoBehaviour
             buttonCountA = 0;
             buttonCountD = 0;
         }
+
+    }
+    private void CheckMovementState()
+    {
+
+        if (buttonCooler > 0 && (buttonCountA > 1 || buttonCountD > 1) && state != State.jumping)
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isWalking = true;
+            buttonCooler = 0.5f;
+        }
+    }
+    private void Movement(float speed)
+    {
         
+        float hDirection = Input.GetAxis("Horizontal");
+
+        if (hDirection < 0)  // Input.GetKeyDown(KeyCode.A) hDirection < 0
+        {
+            transform.localScale = new Vector2(-1, 1);
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        // Move Right
+        if (hDirection > 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        // Double tap checker
+        if ((Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D)) && state != State.jumping)
+        {
+            isRunning = false;
+
+            rb.velocity = new Vector2(0.1f, rb.velocity.y);
+        }
+        
+        
+
     }
 
     private void StateSwitch()
@@ -131,6 +210,15 @@ public class PlayerController : MonoBehaviour
                 state = State.indle;
             }
         }
+        else if (state == State.hurt)
+        {
+
+            if (Mathf.Abs(rb.velocity.x) < 0.5f)
+            {
+                
+                state = State.indle;
+            }
+        }
 
         else if (Mathf.Abs(rb.velocity.x) > 0.5f)
         {
@@ -143,47 +231,21 @@ public class PlayerController : MonoBehaviour
                 state = State.walking;
             }
         }
+        
         else
         {
             state = State.indle;
+            
         }
     }
 
-    private void CheckMovementState()
-    {
-        if (buttonCooler > 0 && (buttonCountA > 1 || buttonCountD > 1) && state != State.jumping)
-        {
-            isRunning = true;
-        }
-        else
-        {
-            isWalking = true;
-            buttonCooler = 0.5f;
-        }
-    }
 
-    private void Movement(float speed)
-    {
-        float hDirection = Input.GetAxis("Horizontal");
-        
-        if (hDirection < 0)  // Input.GetKeyDown(KeyCode.A) hDirection < 0
-        {
-            transform.localScale = new Vector2(-1, 1);
-            rb.velocity = new Vector2(-speed, rb.velocity.y);
-        }
-        // Move Right
-        if (hDirection > 0)
-        {
-            transform.localScale = new Vector2(1, 1);
-            rb.velocity = new Vector2(speed, rb.velocity.y);  
-        }
-        // Double tap checker
-        if((Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D)) && state != State.jumping)
-        {
-            isRunning = false;
 
-            rb.velocity = new Vector2(0.4f,rb.velocity.y);
-        }
-        
+    private void Jumping()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        state = State.jumping;
     }
 }
+
+    
